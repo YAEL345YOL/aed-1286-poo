c9863a2236455f14c35cffa426d05c5d0ad445e3
Lenin Yael Gómez Vega <50465405+YAEL345YOL@users.noreply.github.com>
2026-09-07 10:51:51 -0600

    Clase 07/09/26

--- Proyectos/POO_U2_Bachiller/src/poo_u2_bachiller/Principal.java
+++ Proyectos/POO_U2_Bachiller/src/poo_u2_bachiller/Principal.java
@@ -1,21 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package poo_u2_bachiller;
+import java.util.*;
 
-/**
- *
- * @author ALUMNO PC23LC2
- */
 public class Principal {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Bachiller b1 = new Bachiller();
+        Bachiller b2 = new Bachiller();
+        Bachiller b3 = new Bachiller();
+        Bachiller b4 = new Bachiller();
+        
+        b1.setNombre("Sonia Moreno");
+        b1.setCarrera("Fisico matematico");
+        b1.setCalif(9.5);
+        b1.calcCualificacion();
+        
+        b2.setNombre("Kenia Oz");
+        b2.setCarrera("Musica");
+        b2.setCalif(6.5);
+        b2.calcCualificacion();
+        
+        b3.setNombre("Axel Garcia");
+        b3.setCarrera("Gym");
+        b3.setCalif(5.7);
+        b3.calcCualificacion();
+        
+        b4.setNombre("Leonel Messi");
+        b4.setCarrera("Farmacobiologo");
+        b4.setCalif(9.6);
+        b4.calcCualificacion();
+        
+        
+        // Calcular y mostrar en pantalla el promediode todos
+        
+        // Cuantos son exelentes
+        // Cuantos son suficientes
+        // Nombre del que tiene la mejor calificacion 
+        
+        int exelentes = 0, suficientes = 0, mejorCalif = 0;
+        
+        
     }
-    
 }
